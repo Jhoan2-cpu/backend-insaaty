e01aa421db9c606ae04daa84bd917330581177b4
CRUZ CASTILLO JHOAN ANTONI <116601508+Jhoan2-cpu@users.noreply.github.com>
2026-01-28 23:37:13 -0500

    refactor: Modelos modificados

--- src/modules/auth/auth.controller.spec.ts
+++ src/modules/auth/auth.controller.spec.ts
@@ -11,7 +11,7 @@ describe('AuthController', () => {
 
     controller = module.get<AuthController>(AuthController);
   });
-
+  
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -15,13 +15,20 @@ class LoginDto {
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
-
+  constructor(private readonly authService: AuthService) {}
+  
   @HttpCode(HttpStatus.OK)
   @Post('login')
   async signIn(@Body() signInDto: LoginDto) {
     const user = await this.authService.validateUser(signInDto.email, signInDto.password);
     if (!user) throw new UnauthorizedException('Credenciales incorrectas');
     return this.authService.login(user);
+  }
+
+  // Endpoint para cuando el Access Token caduca
+  @Post('refresh')
+  @HttpCode(HttpStatus.OK)
+  async refresh(@Body() body: { refresh_token: string }) {
+    return this.authService.rotateRefreshToken(body.refresh_token);
   }
 }
